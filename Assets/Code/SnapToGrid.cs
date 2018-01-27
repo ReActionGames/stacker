3f35ad6942339c099abe767e01030ee00a5f7d15
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-01-27 18:56:16 -0500

    Globalized SnapToGrid

--- Assets/Code/SnapToGrid.cs
+++ Assets/Code/SnapToGrid.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
-public class SnapToGrid : MonoBehaviour {
-
-    public TetrisGrid tetrisGrid;
-    [BoxGroup("Snapping")]
-    public Transform objectToSnap;
-
-    [Button(ButtonSizes.Large), BoxGroup("Snapping")]
-    public void Snap()
+namespace Stacker
+{
+    public class SnapToGrid : MonoBehaviour
     {
-        if (tetrisGrid == null || objectToSnap == null)
-            return;
-        var grid = tetrisGrid.Grid;
-        Vector3Int cellPosition = grid.LocalToCell(objectToSnap.localPosition);
-        objectToSnap.localPosition = grid.GetCellCenterLocal(cellPosition);
-    }
 
-    public void Snap(Transform _transform)
-    {
-        if (tetrisGrid == null)
-            return;
-        var grid = tetrisGrid.Grid;
-        Vector3Int cellPosition = grid.LocalToCell(_transform.localPosition);
-        _transform.localPosition = grid.GetCellCenterLocal(cellPosition);
+        public TetroGrid tetrisGrid;
+        [BoxGroup("Snapping")]
+        public Transform objectToSnap;
+
+        [Button(ButtonSizes.Large), BoxGroup("Snapping")]
+        public void Snap()
+        {
+            if (tetrisGrid == null || objectToSnap == null)
+                return;
+            var grid = tetrisGrid.Grid;
+            Vector3Int cellPosition = grid.LocalToCell(objectToSnap.localPosition);
+            objectToSnap.localPosition = grid.GetCellCenterLocal(cellPosition);
+        }
+
+        public void Snap(Transform _transform)
+        {
+            if (tetrisGrid == null)
+                return;
+            var grid = tetrisGrid.Grid;
+            Vector3Int cellPosition = grid.LocalToCell(_transform.localPosition);
+            _transform.localPosition = grid.GetCellCenterLocal(cellPosition);
+        }
     }
 }
