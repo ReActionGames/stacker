8f1fa84b8bb79cf1fe56a12d4be3454fae166f1b
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-02-01 20:08:58 -0500

    Added EventManager

---
+++ Assets/Code/EventManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<string, MessageEvent> eventDictionary;
+
+    private static EventManager eventManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    //private void Awake()
+    //{
+    //    Init();
+    //}
+
+    void Init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, MessageEvent>();
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction<Message> listener)
+    {
+        MessageEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new MessageEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction<Message> listener)
+    {
+        if (instance == null)
+            return;
+        MessageEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName, Message message = null)
+    {
+        MessageEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke(message);
+        }
+    }
+}
+
+[System.Serializable]
+public class MessageEvent : UnityEvent<Message>
+{
+}
---
+++ Assets/Code/Message.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Message
+{
+    public object Data
+    {
+        get; private set;
+    }
+
+    public static Message None
+    {
+        get
+        {
+            return new Message();
+        }
+    }
+
+    public Message()
+    {
+        Data = null;
+    }
+
+    public Message(object data)
+    {
+        Data = data;
+    }
+}
