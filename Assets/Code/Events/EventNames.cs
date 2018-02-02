4ca3bff9581b7c2595186ad4e687a747b7c08293
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-02-01 21:43:48 -0500

    Moved Events Related classes into Events folder

---
+++ Assets/Code/Events/EventNames.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stacker
+{
+    public static class EventNames
+    {
+        public const string InputReceived = "input-received";
+        public const string NewTetroFalling = "new-tetro-falling";
+    }
+}
