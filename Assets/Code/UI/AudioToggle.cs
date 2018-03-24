cb83106e59395345a1e559850275ef583b6515ab
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-03-24 17:43:58 -0400

    Added AudioToggle Button

---
+++ Assets/Code/UI/AudioToggle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Stacker
+{
+    public class AudioToggle : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer masterAudio;
+        [SerializeField] private Image border;
+        [SerializeField] private Image icon;
+        [SerializeField] private ButtonState activeState;
+        [SerializeField] private ButtonState inactiveState;
+
+        [System.Serializable]
+        private class ButtonState
+        {
+            [SerializeField] private Color borderColor;
+            [SerializeField] private Color iconColor;
+
+            public void ApplyState(Image border, Image icon)
+            {
+                border.color = borderColor;
+                icon.color = iconColor;
+            }
+        }
+
+        private bool audioOn = true;
+
+        public void ToggleAudio()
+        {
+            audioOn = !audioOn;
+            float volume = audioOn == true ? 0 : -80;
+            masterAudio.SetFloat("MasterVolume", volume);
+
+            ToggleState();
+        }
+
+        private void ToggleState()
+        {
+            if (audioOn)
+                activeState.ApplyState(border, icon);
+            else
+                inactiveState.ApplyState(border, icon);
+        }
+    }
+}
