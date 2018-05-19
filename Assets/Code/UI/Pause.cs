5be49ccb280a61b0180b0f2c02f529b93ed34ab8
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-05-19 09:50:30 -0400

    Updated Pause script to reflect changes in UI structure

---
+++ Assets/Code/UI/Pause.cs
@@ -0,0 +1,30 @@
+﻿using HenderStudios.Events;
+using HenderStudios.Extensions;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Stacker
+{
+    public class Pause : MonoBehaviour
+    {
+        
+        public void PauseButtonClick()
+        {
+            EventManager.TriggerEvent(EventNames.PauseGame);
+        }
+
+        public void ResumeButtonClick()
+        {
+            EventManager.TriggerEvent(EventNames.ResumeGame);
+        }
+
+        public void HomeButtonClick()
+        {
+            EventManager.TriggerEvent(EventNames.ResumeGame);
+            SceneLoader.LoadScene("Main");
+        }
+    }
+}
