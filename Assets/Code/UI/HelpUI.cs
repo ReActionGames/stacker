d5215cb50532e058dfb8fb163c3fe421012852eb
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-06-06 15:00:11 -0400

    Added Help Screen

---
+++ Assets/Code/UI/HelpUI.cs
@@ -0,0 +1,24 @@
+﻿using DoozyUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stacker
+{
+    public class HelpUI : MonoBehaviour
+    {
+        [SerializeField] private UIElement help1;
+        [SerializeField] private UIElement help2;
+        [SerializeField] private UIElement help3;
+
+        [SerializeField] private UIButton playButton;
+
+        [SerializeField] private Color textColorDisabled;
+        [SerializeField] private Color textColorNormal;
+
+        [SerializeField] private string firstInAnim;
+        [SerializeField] private string lastOutAnim;
+        [SerializeField] private string transitionInAnim;
+        [SerializeField] private string transitionOutAnim;
+    }
+}
